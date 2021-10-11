888713cb8b2cfe5684e2a3aa48223f67fc838b3b
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-11 12:55:37 +0700

    logic kalkulasi di MainActivity.kt

--- app/src/main/java/com/gunder/myviewmodel/MainActivity.kt
+++ app/src/main/java/com/gunder/myviewmodel/MainActivity.kt
@@ -2,13 +2,45 @@ package com.gunder.myviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.gunder.myviewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        displayResult()
+
+        binding.btnCalculate.setOnClickListener {
+            val width = binding.edtWidth.text.toString()
+            val height = binding.edtHeight.text.toString()
+            val length = binding.edtLength.text.toString()
+
+            when{
+                width.isEmpty() -> {
+                    binding.edtWidth.error = "Masih kosong!"
+                }
+                height.isEmpty() -> {
+                    binding.edtHeight.error = "Masih kosong!"
+                }
+                length.isEmpty() -> {
+                    binding.edtLength.error = "Masih kosong!"
+                }
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    displayResult()
+                }
+            }
+        }
+    }
+
+    private fun displayResult() {
+        binding.tvResult.text = viewModel.result.toString()
     }
 }
